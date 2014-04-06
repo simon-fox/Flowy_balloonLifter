0cca49aeaa70c755ccc3c503691e446c3a64f621
simon-fox <thiswayin@gmail.com>
2014-04-06 16:28:30 +0100

    initial commit
    basic prototype

---
+++ todo.js
@@ -0,0 +1,35 @@
+------------------------------
+//////////////////////////////////
+//  FLOWY BALLOON LIFTER todo   //
+//////////////////////////////////
+
+//- Gut out ballon bobble mechanics
+- remove ceiling on level 
+//- balloon, chain and item carried on chain for every breath
+	//- change balloon so it anchors to an item
+	//- item is just small obstacle for now 
+		//- will need to retrofit obstacle class
+//- do we still use anchor and chain?
+//- or do ballons stay in place in center until released?
+	//- using anchor and chain means we can prototype it faster
+
+- fix clean up after breath so it only destroys correct chain. 
+//- make lower chain invisible
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
